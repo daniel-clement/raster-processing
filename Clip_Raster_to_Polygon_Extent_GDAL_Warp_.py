001d66208995ef360f9efa405cabbe5158eff596
Daniel Clement <dclement1928@gmail.com>
2022-02-18 21:43:53 -0500

    Add files via upload

---
+++ Clip_Raster_to_Polygon_Extent_GDAL_Warp_.py
@@ -0,0 +1,64 @@
+# this script was written by Daniel Clement - 2021
+"""
+This script will use GDAL Warp to clip a raster dataset to the extent of a polygon feature
+"""
+
+# do imports
+import os
+import gdal
+import time
+
+# set parameters
+########################################################################################################################
+# the raster you want to clip
+in_raster = r""
+
+# the polygon feature you want to clip the raster to
+clip_feature = r""
+
+# the output clipped raster
+out_raster = r""
+########################################################################################################################
+
+# Start time measurement
+startTime = time.time()
+
+
+def clip_raster(clip_feature, in_raster, out_raster):
+    print("Clipping raster...")
+    # creates the gdal warp command
+    command = "gdalwarp -dstnodata NoData -cutline {} {} {}".format(clip_feature, in_raster, out_raster)
+
+    # runs the command
+    os.system(command)
+
+
+# run the clip raster function
+clip_raster(clip_feature, in_raster, out_raster)
+
+# stop the timer and calculate run time
+executionTimeSec = (time.time() - startTime)
+
+
+def print_execution_time(executionTimeSec):
+    if executionTimeSec < 60:
+        execTimeSecRound = round(executionTimeSec, 2)
+        print("Process completed in {} second(s)".format(execTimeSecRound))
+    elif 60 <= executionTimeSec < 3600:
+        execTimeMin = executionTimeSec / 60
+        execTimeMinRound = int(execTimeMin)
+        execTimeMinRemain = round(executionTimeSec % 60)
+        print("Process completed in {} min {} sec".format(execTimeMinRound, execTimeMinRemain))
+    elif executionTimeSec >= 3600:
+        execTimeHours = executionTimeSec / 3600
+        execTimeHoursRound = int(execTimeHours)
+        execTimeHoursRemain = round((executionTimeSec % 3600) / 60)
+        print("Process completed in {} hour(s) {} min.".format(execTimeHoursRound, execTimeHoursRemain))
+
+
+
+print("\n##################################################################################################")
+print("Raster successfully clipped!")
+print_execution_time(executionTimeSec)
+print("Please find your result at: {}".format(out_raster))
+print("##################################################################################################")
