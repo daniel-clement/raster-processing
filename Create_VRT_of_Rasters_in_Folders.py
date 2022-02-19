f360c5880ceedce06f60577ae35b3813d30697f0
Daniel Clement <dclement1928@gmail.com>
2022-02-18 22:07:57 -0500

    Update Create_VRT_of_Rasters_in_Folders.py

--- Create_VRT_of_Rasters_in_Folders.py
+++ Create_VRT_of_Rasters_in_Folders.py
@@ -1,7 +1,7 @@
 # This script was written by Daniel Clement - 2021
 # Python 3.7
 """
-this script will make a VRT from all of the images inside of a folder, even if they are in sub-folders.
+this script will make a VRT from all of the rasters inside of a folder, even if they are in sub-folders.
 """
 
 # do imports
@@ -10,27 +10,27 @@ import glob2 as glob
 import gdal
 
 #######################################################################################################################
-# folder with the tif files
+# folder with the raster files
 inDir = r""
 
 # output VRT file
 outVrt = r""
 
-# input image format
+# input rastervformat
 inImgFormat = "tif"  # "tif" or "img"
 #######################################################################################################################
 
 
 # get path of all tifs in the sub-folders of the inDir
-tifList = glob.glob(r"{}\**\*.{}".format(inDir, inImgFormat))
+rasterList = glob.glob(r"{}\**\*.{}".format(inDir, inImgFormat))
 
-print("\nFound {} images...".format(len(tifList)))
+print("\nFound {} rasters...".format(len(tifList)))
 
 # create txt file with list of tifs in input folder
 outTxt = os.path.join(inDir, "ImageList.txt")
 with open(outTxt, 'w') as f:
-    for tif in tifList:
-        f.write("{}\n".format(tif))
+    for raster in rasterList:
+        f.write("{}\n".format(raster))
 
 # create a vrt from the tifs in the outTxt file
 print("\nCreating VRT...")
