bcf54704ca9024af84429524bf6ca4a6d091e23b
Daniel Clement <dclement1928@gmail.com>
2022-03-31 00:07:38 -0400

    Update Image_Contrast_Stretching_PIL.py

--- Image_Contrast_Stretching_PIL.py
+++ Image_Contrast_Stretching_PIL.py
@@ -19,23 +19,24 @@ inDir = r"c:\data\original_images"
 # the folder where you want your processed images to be saved to
 outDir = r"c:\data\adjusted_images"
 
-# A contrast enhancement factor of 0.0 gives a solid grey image. A factor of 1.0 gives the original image.
+# A contrast enhancement factor of 0.0 gives a solid grey image. 
+# A factor of 1.0 gives the original image.
 contrast_Factor = 1.5
 
-# A brightness enhancement factor of 0.0 gives a black image. A factor of 1.0 gives the original image.
+# A brightness enhancement factor of 0.0 gives a black image. 
+# A factor of 1.0 gives the original image.
 brightness_Factor = 1.5
 
-# A sharpness enhancement factor of 0.0 gives a fully blured image. A factor of 1.0 gives the original image. And a
-# factor of 2 gives a sharpened image.
+# A sharpness enhancement factor of 0.0 gives a fully blured image. 
+# A factor of 1.0 gives the original image. And a factor of 2 gives a sharpened image.
 sharpness_Factor = 1.5
 
+# the file format of the input images
 input_format = "jpg"  # "jpg" or "tif" or "png"
 
+# the file format you want the output images to be
 output_format = "tif"  # "jpg" or "tif" or "png"
 #######################################################################################################################
-
-# get list of all images in inDir
-inImgs = glob(inDir + "/*.{}".format(input_format))
 
 
 def adjustment_process(input_image, contrast_factor, brightness_factor, sharpness_factor, out_dir):
@@ -49,7 +50,7 @@ def adjustment_process(input_image, contrast_factor, brightness_factor, sharpnes
     :return:
     """
 
-    # open the image with PIL
+   # open the image with PIL
     img = Image.open(input_image)
 
     # perform the contrast adjustment
@@ -65,32 +66,45 @@ def adjustment_process(input_image, contrast_factor, brightness_factor, sharpnes
     manSharpImg = obj3.enhance(sharpness_factor)
 
     # create output path
-    outImg = os.path.join(out_dir, os.path.basename(input_image).replace(".{}".format(input_format),
-                                                                         "_adjusted.{}".format(output_format)))
+    # get the input image name
+    inFileName = os.path.basename(input_image)
 
-    # write the image to disk, in the desired output format
-    if output_format == "jpg":
-        # write image to disk
-        manSharpImg.save(outImg, "JPEG")
-    elif output_format == "tif":
-        # write image to disk
-        manSharpImg.save(outImg, "TIFF")
-    elif output_format == "png":
-        manSharpImg.save(outImg, "PNG")
+    # create the output files name by adding "_adjusted" to the end of the input file's name,
+    # and make the file format equal the output_format parameter
+    outFileName = inFileName.replace(".{}".format(input_format), 
+                                     "_adjusted.{}".format(output_format))
 
+    # join the output folder and the output file name to create the output file path
+    outImg = os.path.join(out_dir, outFileName)
+
+    # write the adjusted image to the disk
+    manSharpImg.save(outImg)
+
+
+# get list of all images in inDir
+inImgs = glob(inDir + "/*.{}".format(input_format))
 
 # for image in inImgs list, adjust images, and save the outputs
-for i in tqdm(inImgs):
+for image in tqdm(inImgs):
 
-    adjustment_process(i, contrast_Factor, brightness_Factor, sharpness_Factor, outDir)
+    # run the adjustment processor function on the image
+    adjustment_process(input_image=image, 
+                       contrast_factor=contrast_Factor, 
+                       brightness_factor=brightness_Factor, 
+                       sharpness_factor=sharpness_Factor, 
+                       out_dir=outDir)
 
-# get number of input and output images
+# use glob to get a list of all of the output images
 outImgs = glob(outDir + "/*.{}".format(output_format))
+
+# use the len function to get the number of files in the outImgs list
 numOutImgs = len(outImgs)
+
+# use the len function to get the number of files in the inImgs list
 numImgs = len(inImgs)
 
 # ensure the number of output images matches the number of input images
 if numImgs == numOutImgs:
     print("All images processed successfully!!!")
-else:
+elif numImgs > numOutImgs:
     print("ERROR - All images not processed successfully :'(")
